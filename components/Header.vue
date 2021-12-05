191916fe427a3b844c5e76a268edb7a2f309b73f
Gabriel Cardoso <68553837+GabrielCardoso-psd@users.noreply.github.com>
2021-12-05 13:32:41 -0300

    up

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,12 @@
 <template>
 <div class="background">
   <div class="container">
-    <img src="" alt="">
+    <b-row>
+    <img src="../assets/img/car-icon-3657902_960_720.png" alt="">
+      <div class="nav-bar nav">
+        <div class="nav-bar-links"></div>
+      </div>
+    </b-row>
   </div>
 </div>
 </template>
