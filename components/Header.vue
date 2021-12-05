16b62e162970ffdcd53e58a81358e0b4b48fc920
Gabriel Cardoso <68553837+GabrielCardoso-psd@users.noreply.github.com>
2021-12-05 13:48:56 -0300

    up

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,17 @@
 <div class="background">
   <div class="container">
     <b-row>
-    <img src="../assets/img/car-icon-3657902_960_720.png" alt="">
+    <img class="logo" src="../assets/img/car-icon-3657902_960_720.png" alt="">
       <div class="nav-bar nav">
-        <div class="nav-bar-links"></div>
+        <div id="navbar-links">
+          <div class="navbar-nav">
+            <a class="nav-link" href="">Destaques da semana</a>
+            <a class="nav-link" href="">Financiamento</a>
+            <a class="nav-link" href="">Clientes</a>
+            <a class="nav-link" href="">Simulação</a>
+            <b-button>Faça sua ficha!</b-button>
+          </div>
+        </div>
       </div>
     </b-row>
   </div>
@@ -18,5 +26,7 @@ export default {
 </script>
 
 <style scoped>
-
+.logo{
+  width: 150px;
+}
 </style>
