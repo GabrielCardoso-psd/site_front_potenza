273c6d68a4c0c4c2b2fd94625325693b1e5eb4b7
projetodevnx <gabrielcrs2016@gmail.com>
2021-12-08 14:21:59 -0300

    up

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,6 +12,8 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
+import nuxt_plugin_plugin_9c68ff00 from 'nuxt_plugin_plugin_9c68ff00' // Source: .\\components\\plugin.js (mode: 'all')
+
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
 
@@ -60,7 +62,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"meta":[],"link":[],"style":[],"script":[]},
+    head: {"title":"site_front_potenza","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""},{"name":"format-detection","content":"telephone=no"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
     router,
     nuxt: {
@@ -173,6 +175,10 @@ async function createApp(ssrContext, config = {}) {
     }
   }
   // Plugin execution
+
+  if (typeof nuxt_plugin_plugin_9c68ff00 === 'function') {
+    await nuxt_plugin_plugin_9c68ff00(app.context, inject)
+  }
 
   // Lock enablePreview in context
   if (process.static && process.client) {
--- components/Header.vue
+++ components/Header.vue
@@ -2,18 +2,21 @@
   <div>
     <div class="background">
       <div class="container">
+        <b-row>
+          <b-col>
         <div>
           <img class="img" src="../assets/img/car-icon-3657902_960_720.png" alt="">
         </div>
-        <div id="navbar-links">
+          </b-col>
+        <div>
           <div class="navbar-nav">
-            <a class="nav-link" href="#camuflagem">Destaques da semana</a>
-            <a class="nav-link" href="#procedimentos">Financiamento</a>
-            <a class="nav-link" href="#contato">Clientes</a>
-            <a class="nav-link" href="#contato">Simulação</a>
-            <a href="" target="_blank"><b-button class="botao"><span class="icones fa-lg"><font-awesome-icon :icon="['fab', 'whatsapp']"/></span>Faça sua ficha!</b-button></a>
+            <a class="nav-link" href="#">Destaques da semana</a>
+            <a class="nav-link" href="#">Destaques da semana</a>
+            <a class="nav-link" href="#">Destaques da semana</a>
+            <a class="nav-link" href="#">Destaques da semana</a>
           </div>
         </div>
+        </b-row>
       </div>
     </div>
   </div>
